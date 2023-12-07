c00437daa5b25effe90b3ed0417d3ffe17371f5d
Shashidhar Gurumurthy <shashidhar.gurumurthy@gmail.com>
2023-12-07 08:42:34 +0530

    Exercise 06-Dec-2023

---
+++ src/exercise-06Dec2023.js
@@ -0,0 +1,19 @@
+const companies = [
+  {name: 'Intelliswift', hq: 'Newark, CA', indiaLocations: ['Pune', 'Bengaluru']},
+  {name: 'Salesforce', hq: 'San Francisco, CA', indiaLocations: ['Hyderabad', 'Bengaluru', 'Mumbai', 'Gurugram', 'Jaipur']}
+];
+
+/* 
+  Task-1 
+  Print following company information on console
+  1. Number of companies, 
+  2. Name of companies, 
+  3. Headquarters of the companies
+
+  Task-2
+  Have a button in HTML and on click of the button, show the following as a bulleted list on HTML page:
+  Name of company - HQ - India Locations
+  E.g. Intelliswift - Newark, CA - Pune, Bengaluru
+
+  Hint: You will need to use document.querySelector or document.getElementById and set it's inner HTML to the content.
+*/
