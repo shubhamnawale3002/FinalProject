8990a1b3857610633dac899f2cbaaf3ef1c12deb
Shashidhar Gurumurthy <shashidhar.gurumurthy@gmail.com>
2023-12-08 14:49:59 +0530

    Some changes

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-"use strict"
-let data
-
-import('./src/moviesPlay.js')
-	.then(res => {
-		console.log('data imported into data constant');
-		data = res;
-	});
-
-function inputChanged(event) {
-	document.getElementById('output').innerHTML = event.target.value;
-}
-
-function onSubmit(event) {
-	event.preventDefault();
-	console.log('Form Submitted'); 
-}
-
-function showCounts() {
-	console.log(data.getCounts());
-}
+import { movies } from "./data/movies";
+movies.forEach(function(movie) {
+	console.log(movie.title);
+})
